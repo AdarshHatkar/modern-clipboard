39ec4b2a69acf0ac1e2673e21f961f54b0d4f5f4
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-05-11 13:17:57 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # modern-clipboard
 Simple copy to clipboard function with great compatibility 
+
+[![npm Package](https://img.shields.io/npm/v/modern-clipboard.svg)](https://www.npmjs.org/package/modern-clipboard)
+[![License](https://img.shields.io/npm/l/modern-clipboard.svg)](https://github.com/AdarshHatkar/modern-clipboard/blob/main/LICENSE)
+
+
+Installation
+------------
+
+    npm i modern-clipboard
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+function fallbackCopyTextToClipboard(text: string) {
+    try {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        const successful = document.execCommand('copy');
+        const msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+        document.body.removeChild(textArea);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+}
+
+export const copyTextToClipboard = (text: string) => {
+    const clipboard = navigator.clipboard;
+
+    if (!clipboard?.writeText) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+
+    clipboard.writeText(text).then(() => {
+        console.log('Async: Copying to clipboard was successful!');
+    }).catch((err) => {
+        console.error('Async: Could not copy text: ', err);
+        fallbackCopyTextToClipboard(text);
+    });
+};
